241cbec0f94b8b2e9957758da178a1d6179b6369
Tiptis Aura <aura.tiptis@gmail.com>
2018-03-06 16:24:19 +0200

    added style on admin.php

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="canonical" content="">
 	<meta name="description" content="">
-	<title>Zna de autentificare</title>
+	<title>Zona de autentificare</title>
 	<link href="html/css/bootstrap.min.css" rel="stylesheet">
 	<link href="html/css/style.css" rel="stylesheet">
 
@@ -15,7 +15,29 @@
 	<body>
 		<div class="container">
 			<div class="row">
-				<div class="title"> Zona de autentificare</div>
+				<div class="col">
+					<div class="header">Zona de autentificare</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="autentificare">
+					<form>
+					
+					  <div class="form-group">
+					    <input type="email" class="form-control input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					  </div>
+
+					  <div class="form-group">
+					    <input type="password" class="form-control input" id="exampleInputPassword1" placeholder="Password">
+					  </div>
+
+					  <button type="submit" class="btn btn-primary btn-block  submit">Submit</button>
+
+					</form>
+				</div>
 			</div>
 		</div>
 	</body>
