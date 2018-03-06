143c520ea91e95f123320c058e6e1f914068d67d
Tiptis Aura <aura.tiptis@gmail.com>
2018-03-06 14:46:44 +0200

    added resources folder and amin conect page

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="canonical" content="">
+	<meta name="description" content="">
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
