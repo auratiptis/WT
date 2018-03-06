fbe40ab490e8aa553ac64b7a159485352687c179
Tiptis Aura <aura.tiptis@gmail.com>
2018-03-06 14:51:19 +0200

    include bootstrap to admin.php

--- admin.php
+++ admin.php
@@ -5,7 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="canonical" content="">
 	<meta name="description" content="">
-	<title></title>
+	<title>Zna de autentificare</title>
+	<link href="html/css/bootstrap.min.css" rel="stylesheet">
+	<link href="html/css/style.css" rel="stylesheet">
+
+
 </head>
 <body>
 
